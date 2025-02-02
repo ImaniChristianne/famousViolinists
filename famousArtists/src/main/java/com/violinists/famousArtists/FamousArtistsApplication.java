8dbc693757e146dc6959a6284e3ba2ac2b4cdfe2
Christianne1 <imaene@iu.edu>
2025-02-02 01:39:16 -0500

    Initial commit

---
+++ famousArtists/src/main/java/com/violinists/famousArtists/FamousArtistsApplication.java
@@ -0,0 +1,13 @@
+package com.violinists.famousArtists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamousArtistsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamousArtistsApplication.class, args);
+	}
+
+}
---
+++ famousArtists/src/test/java/com/violinists/famousArtists/FamousArtistsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.violinists.famousArtists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamousArtistsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
